7fa957aab4d73246615f3edca85c96dd2db63191
807715333@qq.com <807715333@qq.com>
2020-05-12 18:51:23 +0800

    'scalaio'

--- src/LeetCode/Scala/scala/IODemo/SourceDemo.scala
+++ src/LeetCode/Scala/scala/IODemo/SourceDemo.scala
@@ -5,8 +5,10 @@ import scala.io.Source
 object SourceDemo {
   def main(args: Array[String]): Unit = {
 //    fromFile得到的结果进行foreach是针对每一个字符的
-    Source.fromFile("F:\\Thread.txt","utf-8").foreach(print)
-    val list = Source.fromFile("F:\\Thread.txt","utf-8").toList
-    println(list)
+//    Source.fromFile("F:\\Thread.txt","utf-8").foreach(print)
+    val regex="(?<=from )\\btbl_\\w*\\b".r
+    val str=Source.fromFile("E:\\test.java").mkString
+    (regex findAllIn str).toList.distinct.foreach(println)
+
   }
 }
